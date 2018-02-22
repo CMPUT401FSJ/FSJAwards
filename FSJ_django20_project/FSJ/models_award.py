4252042ff9f32af80ca199af7991061d71151c00
Pierre Lin <pslin@ualberta.ca>
2018-02-21 20:41:04 -0700

    class Award(models.Model) can't find Model

--- FSJ_django20_project/FSJ/models_award.py
+++ FSJ_django20_project/FSJ/models_award.py
@@ -1,13 +1,47 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from .forms import AddAwardForm
+from django.forms import *
 import datetime
 
 class Award(models.Model):
-	name = models.CharField(max_length = 255, verbose_name = _("Name"))
+	FIRST = 1
+	SECOND = 2
+	THIRD = 3
+	FOURTH = 4
+	FIFTH = 5
+	YEAR_CHOICES = (
+		(FIRST, _('First')),
+		(SECOND, _('Second')),
+		(THIRD, _('Third')),
+		(FOURTH, _('Fourth')),
+		(FIFTH, _('Fifth'))
+	)
+	#awardid = models.PositiveIntegerField(unique = True, verbose_name = _("Award ID"), default=1)
+	award_name = models.TextField(verbose_name = _("Award Name"))
 	description = models.TextField(verbose_name = _("Description"))
 	value = models.PositiveIntegerField(verbose_name = _("Value"))
 	programs = models.TextField(verbose_name = _("Programs"))
-	years_of_study = models.TextField(verbose_name = _("Years of Study"))
+	years_of_study = models.IntegerField(choices = YEAR_CHOICES, verbose_name = _("Years of Study"))
 	deadline = models.DateTimeField(auto_now = False, auto_now_add = False, verbose_name = _("Deadline"))
 	documents_needed = models.BooleanField(verbose_name = _("Documents Required"))
+	#document = models.FileField(upload_to = None, max_length = 100, verbose_name = _("Documents"))
 	is_active = models.BooleanField(verbose_name = ("Is Active"))
+
+
+	def __str__(self):
+		return self.awardid
+
+	def get_add_award_form(self, request = None):
+		if request and request.POST:
+			add_award_form = CoordinatorAddAwardForm(request.POST)
+		else:
+			add_award_form = CoordinatorAddAwardForm()
+		return add_award_form
+
+class CoordinatorAddAwardForm(AddAwardForm):
+	award_id = IntegerField()
+       
+	def __init__(self, *args, **kwargs):
+		super(AddAwardForm, self).__init__(*args, **kwargs)           
+		self.fields['award_id'].disabled = True
