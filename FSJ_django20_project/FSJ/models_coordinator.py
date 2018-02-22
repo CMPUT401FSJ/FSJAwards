351b86845fe707eb69d4cb007c89f99ef6a5e28f
jrforest <jrforest@ualberta.ca>
2018-02-22 11:40:29 -0700

    Cleaning up

--- FSJ_django20_project/FSJ/models_coordinator.py
+++ FSJ_django20_project/FSJ/models_coordinator.py
@@ -1,1 +1,1 @@
-from django.db import modelsfrom .models_FSJUser import FSJUser#from .forms import ProfileFormfrom django.utils.translation import gettext_lazy as _from django.forms import *class Coordinator(FSJUser):    def user_class(self):        return "Coordinator"    
+from django.db import modelsfrom .models_FSJUser import FSJUserfrom django.utils.translation import gettext_lazy as _from django.forms import *class Coordinator(FSJUser):    def user_class(self):        return "Coordinator"    
