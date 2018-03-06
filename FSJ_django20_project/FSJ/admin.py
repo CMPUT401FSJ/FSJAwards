e770ff05285210eb2d5da052ca7b7714060c5a35
dkotowic <kotowicz@ualberta.ca>
2018-03-06 16:26:54 -0700

    remove all files (to be readded after)

--- FSJ_django20_project/FSJ/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import *
-
-# Allow the admin site to maintain students, coordinators, adjudicators
-admin.site.register(Student)
-admin.site.register(Coordinator)
-admin.site.register(Adjudicator)
-admin.site.register(Award)
--- FSJ_django20_project/FSJ/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FsjConfig(AppConfig):
-    name = 'FSJ'
