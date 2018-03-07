d264bd9698dd159ca0a66e4d68301e74a3207b50
jrforest <jrforest@ualberta.ca>
2018-03-06 20:22:16 -0700

    Now have application adding and viewing

--- FSJ_django20_project/FSJ/models_application.py
+++ FSJ_django20_project/FSJ/models_application.py
@@ -4,9 +4,15 @@ from .models_award import Award
 from .models_student import Student
 
 class Application(models.Model):
-    award = models.ForeignKey(Award, on_delete=models.CASCADE)
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    award = models.ForeignKey(Award, on_delete=models.CASCADE, related_name='applications')
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='applications')
     is_submitted = models.BooleanField(verbose_name = _("Is Submitted"))
     
     def __str__(self):
-        return self.award.award_name    
+        return self.student.ccid + "'s application for " + self.award.award_name    
+    
+    def getawardname(self):
+        return self.award.award_name
+    
+    def getstudentccid(self):
+        return self.student.ccid
