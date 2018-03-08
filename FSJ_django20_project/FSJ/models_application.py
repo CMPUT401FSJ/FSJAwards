729f3e7143828af1d0c3f29f88152906a432a02e
jrforest <jrforest@ualberta.ca>
2018-03-08 12:33:03 -0700

    Fixing problems in application model

--- FSJ_django20_project/FSJ/models_application.py
+++ FSJ_django20_project/FSJ/models_application.py
@@ -1,11 +1,13 @@
+import uuid
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from .models_award import Award
 from .models_student import Student
 
 class Application(models.Model):
+    application_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     award = models.ForeignKey(Award, on_delete=models.CASCADE, related_name='applications')
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='applications')
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, to_field='ccid', related_name='applications')
     is_submitted = models.BooleanField(verbose_name = _("Is Submitted"))
     
     def __str__(self):
