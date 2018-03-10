8b304cc1fcc6026e2e3256b6674e1c17d5e809a5
Pierre Lin <pslin@ualberta.ca>
2018-03-10 14:06:06 -0700

    years of study now unique, textfield, not limited by YEAR_CHOICES

--- FSJ_django20_project/FSJ/models_yearofstudy.py
+++ FSJ_django20_project/FSJ/models_yearofstudy.py
@@ -3,25 +3,12 @@ from django.db import models
 
 class YearOfStudy(models.Model):
 	#Choices of year for year of study
-	FIRST = 1
-	SECOND = 2
-	THIRD = 3
-	FOURTH = 4
-	FIFTH = 5
-	YEAR_CHOICES = (
-		(FIRST, _('First')),
-		(SECOND, _('Second')),
-		(THIRD, _('Third')),
-		(FOURTH, _('Fourth')),
-		(FIFTH, _('Fifth'))
-	)
-	year = models.IntegerField(choices = YEAR_CHOICES, verbose_name = _("Year of Study"))
+	year = models.CharField(max_length = 255, unique = True, verbose_name = _("Year of Study"))
 
 	#Even if a year is deleted and readded, they will still appear in order
 	class Meta:
 		ordering = ('year', )
 
-	#years appear as "First", "Second", etc instead of 1, 2, etc 
 	def __str__(self):
-		return self.get_year_display()
+		return self.year
 
