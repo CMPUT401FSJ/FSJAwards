e0d9929a94e3e165a14e3ff9c313262536e3a1c7
Pierre Lin <pslin@ualberta.ca>
2018-03-10 16:12:36 -0700

    removed ordering from years of study choices

--- FSJ_django20_project/FSJ/models_yearofstudy.py
+++ FSJ_django20_project/FSJ/models_yearofstudy.py
@@ -5,10 +5,5 @@ class YearOfStudy(models.Model):
 	#Choices of year for year of study
 	year = models.CharField(max_length = 255, unique = True, verbose_name = _("Year of Study"))
 
-	#Even if a year is deleted and readded, they will still appear in order
-	class Meta:
-		ordering = ('year', )
-
 	def __str__(self):
 		return self.year
-
