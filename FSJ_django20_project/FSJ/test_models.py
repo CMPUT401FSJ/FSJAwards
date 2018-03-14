6e101a6c24626c31fe1ec977f20dd657983128a6
jrforest <jrforest@ualberta.ca>
2018-03-14 15:28:00 -0600

    Student model tests completed and run successfully

--- FSJ_django20_project/FSJ/test_models.py
+++ FSJ_django20_project/FSJ/test_models.py
@@ -105,37 +105,62 @@ class CoordinatorModelTests(TestCase):
         with self.assertRaises(User.DoesNotExist):
             user = User.objects.get(username = self.ccid)
 
-# Create your tests here.
 
 class StudentModelTests(TestCase):
     
     def setUp(self):
-        # Link FSJUser with a User model for authentication related business
-        user = User.objects.create(username = "TestStudent", password = "studentpassword")    
-        program = Program.objects.create(code = "SP", name = "StudentProgram")
-        year = YearOfStudy.objects.create(year = "StudentYear")
-        student = Student.objects.create(user = user, ccid = "TestStudent", first_name = "A", last_name = "Student", email = "astudent@test.com", ualberta_id = 1,
-                               year = year, program = program)
+        
+        self.ccid = "Student"
+        self.first_name = "A"
+        self.last_name = "Student"
+        self.email = "aStudent@test.com"
+        self.ualberta_id = 1
+        self.programcode = "SP"
+        self.programname = "StudentProgram"
+        self.yearname = "StudentYear"
+        
+        program = Program.objects.create(code = self.programcode, name = self.programname)
+        year = YearOfStudy.objects.create(year = self.yearname)
+        
+        Student.objects.create(ccid = self.ccid, first_name = self.first_name, last_name = self.last_name, 
+                               email = self.email, ualberta_id = self.ualberta_id, year = year, program = program)        
         
         
-    def test_student_retrieval(self):
-        user = User.objects.get(username = "TestStudent")    
-        program = Program.objects.get(code = "SP")
-        year = YearOfStudy.objects.get(year = "StudentYear")
-        student = Student.objects.get(ccid = "TestStudent")        
+    def test_get_student(self):
+        obj = Student.objects.get(ccid = self.ccid)
+        program = Program.objects.get(code = self.programcode)
+        year = YearOfStudy.objects.get(year = self.yearname)
         
-        self.assertEqual(student.ccid, "TestStudent")
-        self.assertEqual(student.program, program)
-        self.assertEqual(student.ccid, student.user.username)
-        self.assertEqual(student.year, year)
+        self.assertEqual(obj.ccid, self.ccid)
+        self.assertEqual(obj.first_name, self.first_name)
+        self.assertEqual(obj.last_name, self.last_name)
+        self.assertEqual(obj.email, self.email)
+        self.assertEqual(obj.ualberta_id, self.ualberta_id)
+        self.assertEqual(obj.program, program)
+        self.assertEqual(obj.year, year)
         
+    def test_create_duplicate_student(self):  
+        with self.assertRaises(IntegrityError):
+            Student.objects.create(ccid = self.ccid)
+        with self.assertRaises(IntegrityError):
+            Student.objects.create(ualberta_id = self.ualberta_id)               
+        
+    def test_user_model_is_created_with_student(self):
+        obj = Student.objects.get(ccid = self.ccid)
+        user = obj.user
+        self.assertEqual(user.username, obj.ccid)    
+        
+    def test_user_model_is_correct_for_student(self):
+        obj = Student.objects.get(ccid = self.ccid)
+        user = obj.user
+        user2 = User.objects.get(username = self.ccid)
+        self.assertEqual(user, user2)    
         
     def test_program_delete(self):
-        user = User.objects.get(username = "TestStudent")           
-        year = YearOfStudy.objects.get(year = "StudentYear")
-        Program.objects.get(code = "SP").delete()
         
-        student = Student.objects.get(ccid = "TestStudent")  
+        Program.objects.get(code = self.programcode).delete()
+        
+        student = Student.objects.get(ccid = self.ccid)  
         self.assertIsNone(student.program)
         
     
@@ -143,4 +168,17 @@ class StudentModelTests(TestCase):
         
         with self.assertRaises(ProtectedError):
             YearOfStudy.objects.get(year = "StudentYear").delete()
+            
+            
+    def test_delete_coordinator(self):
+        obj = Student.objects.get(ccid = self.ccid)
+        ccid = obj.ccid
+        self.assertIsNotNone(obj)
+        user = User.objects.get(username = self.ccid)
+        self.assertIsNotNone(user)
+        obj.delete()
+        with self.assertRaises(Student.DoesNotExist):
+            obj = Student.objects.get(ccid = self.ccid)
+        with self.assertRaises(User.DoesNotExist):
+            user = User.objects.get(username = self.ccid)    
             
