f8a737012ceab2fb87ec47164dec72517c30517b
jrforest <jrforest@ualberta.ca>
2018-03-14 12:54:42 -0600

    adding some tests

--- FSJ_django20_project/FSJ/tests.py
+++ FSJ_django20_project/FSJ/tests.py
@@ -1,14 +1,24 @@
 from django.test import TestCase
-from .models import Student
-from .models import Program
-from .models import YearOfStudy
+from .models_student import Student
+from .models_program import Program
+from .models_yearofstudy import YearOfStudy
+from django.contrib.auth.models import User
 
 # Create your tests here.
 
 class StudentModelTests(TestCase):
     
     def setUp(self):
-        Program.objects.create(code = "SP", name = "StudentProgram")
-        YearOfStudy.objects.create(year = "StudentYear")
-        Student.objects.create(ccid = "Student", first_name = "A", last_name = "Student", email = "astudent@test.com", ualberta_id = 1,
-                               year = "F1", program = "P1")
+        # Link FSJUser with a User model for authentication related business
+        self.user = User.objects.create(username = "Student")    
+        self.program = Program.objects.create(code = "SP", name = "StudentProgram")
+        self.year = YearOfStudy.objects.create(year = "StudentYear")
+        self.student = Student.objects.create(user = self.user, ccid = "Student", first_name = "A", last_name = "Student", email = "astudent@test.com", ualberta_id = 1,
+                               year = self.year, program = self.program)
+        
+        
+    def test_student_retrieval(self):
+        self.assertEqual(self.student.ccid, "Student")
+        self.assertEqual(self.student.program, self.program)
+        self.assertEqual(self.student.ccid, self.student.user.username)
+        self.assertEqual(self.student.year, self.year)
