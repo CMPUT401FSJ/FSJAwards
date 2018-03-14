8286808ca4bf1c48be142c954f2a961d1cffef59
dkotowic <kotowicz@ualberta.ca>
2018-03-14 14:56:19 -0600

    implement some basic profile tests

--- FSJ_django20_project/FSJ/test.py
+++ FSJ_django20_project/FSJ/test.py
@@ -1,1 +1,3 @@
 from .tests.test_models import *
+from .tests.test_views import *
+from .tests.test_forms import *
--- FSJ_django20_project/FSJ/tests/test_views.py
+++ FSJ_django20_project/FSJ/tests/test_views.py
@@ -1,1 +1,63 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import AnonymousUser, User
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponseRedirect
+from ..views import profile
+from ..models import Student, YearOfStudy
+
+class ProfileViewTests(TestCase):
+    
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.username = "JOE"
+        self.email = "Joe@Joe.ca"
+        self.password = "myPass!"
+        self.url = '/FSJ/profile'
+        self.user = User.objects.create_user(username = self.username, email = self.email, password = self.password)
+        self.ccid = "Student"
+        self.first_name = "A"
+        self.last_name = "Student"
+        self.email = "aStudent@test.com"
+        self.ualberta_id = 1
+        self.year = "First"
+        self.year_of_study = YearOfStudy.objects.create(year = self.year)
+        self.student = Student.objects.create(ccid = self.ccid, first_name = self.first_name, 
+                                   last_name = self.last_name, email = self.email, ualberta_id = self.ualberta_id, year = self.year_of_study)
+        
+    def test_not_logged_in(self):
+        request = self.factory.get(self.url)
+        request.user = AnonymousUser()
+        response = profile(request)
+        # redirect to login page
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/FSJ/login/?next=' + self.url)
+        
+    def test_get_logged_in_without_FSJ_user(self):
+        request = self.factory.get(self.url)
+        request.user = self.user
+        self.assertEqual(request.method, "GET")
+        # User without corresponding FSJ User gets permission denied due to decorator
+        with self.assertRaises(PermissionDenied):
+            response = profile(request)
+            
+    def test_post_logged_in_without_FSJ_user(self):
+        request = self.factory.post(self.url)
+        request.user = self.user
+        self.assertEqual(request.method, "POST")
+        # User without corresponding FSJ User gets permission denied due to decorator
+        with self.assertRaises(PermissionDenied):
+            response = profile(request)            
+        
+    def test_get_logged_in_with_Student(self):
+        request = self.factory.get(self.url)
+        request.user = self.student.user
+        self.assertEqual(request.method, "GET")
+        response = profile(request)
+        self.assertEqual(response.status_code, 200)
+        
+    def test_post_logged_in_with_Student(self):
+        request = self.factory.post(self.url)
+        request.user = self.student.user
+        self.assertEqual(request.method, "POST")
+        response = profile(request)
+        self.assertEqual(response.status_code, 200)    
