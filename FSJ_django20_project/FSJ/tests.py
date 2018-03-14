6f88fd7d97940dbff3736ac1ef965cf60bdfd1f6
jrforest <jrforest@ualberta.ca>
2018-03-14 13:38:02 -0600

    new student tests

--- FSJ_django20_project/FSJ/tests.py
+++ FSJ_django20_project/FSJ/tests.py
@@ -3,6 +3,8 @@ from .models_student import Student
 from .models_program import Program
 from .models_yearofstudy import YearOfStudy
 from django.contrib.auth.models import User
+from django.test import Client
+from django.db.models.deletion import ProtectedError
 
 # Create your tests here.
 
@@ -10,15 +12,37 @@ class StudentModelTests(TestCase):
     
     def setUp(self):
         # Link FSJUser with a User model for authentication related business
-        self.user = User.objects.create(username = "Student")    
-        self.program = Program.objects.create(code = "SP", name = "StudentProgram")
-        self.year = YearOfStudy.objects.create(year = "StudentYear")
-        self.student = Student.objects.create(user = self.user, ccid = "Student", first_name = "A", last_name = "Student", email = "astudent@test.com", ualberta_id = 1,
-                               year = self.year, program = self.program)
+        user = User.objects.create(username = "TestStudent", password = "studentpassword")    
+        program = Program.objects.create(code = "SP", name = "StudentProgram")
+        year = YearOfStudy.objects.create(year = "StudentYear")
+        student = Student.objects.create(user = user, ccid = "TestStudent", first_name = "A", last_name = "Student", email = "astudent@test.com", ualberta_id = 1,
+                               year = year, program = program)
         
         
     def test_student_retrieval(self):
-        self.assertEqual(self.student.ccid, "Student")
-        self.assertEqual(self.student.program, self.program)
-        self.assertEqual(self.student.ccid, self.student.user.username)
-        self.assertEqual(self.student.year, self.year)
+        user = User.objects.get(username = "TestStudent")    
+        program = Program.objects.get(code = "SP")
+        year = YearOfStudy.objects.get(year = "StudentYear")
+        student = Student.objects.get(ccid = "TestStudent")        
+        
+        self.assertEqual(student.ccid, "TestStudent")
+        self.assertEqual(student.program, program)
+        self.assertEqual(student.ccid, student.user.username)
+        self.assertEqual(student.year, year)
+        
+        
+    def test_program_delete(self):
+        user = User.objects.get(username = "TestStudent")           
+        year = YearOfStudy.objects.get(year = "StudentYear")
+        Program.objects.get(code = "SP").delete()
+        
+        student = Student.objects.get(ccid = "TestStudent")  
+        self.assertIsNone(student.program)
+        
+    
+    def test_year_delete(self):
+        
+        with self.assertRaises(ProtectedError):
+            YearOfStudy.objects.get(year = "StudentYear").delete()
+            
+        
