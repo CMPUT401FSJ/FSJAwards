3193204eb00551a21dba965a74d7850c78737db5
jocowan <jcowan@ualberta.ca>
2018-03-13 23:33:27 -0600

    beautified filters, collapse functionality not yet working

--- FSJ_django20_project/FSJ_django20_project/settings.py
+++ FSJ_django20_project/FSJ_django20_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_filters'
+    'django_filters',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE = [
