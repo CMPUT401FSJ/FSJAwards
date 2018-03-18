ddee4377e17763652e02b459f814de4f0f21a0ca
jrforest <jrforest@ualberta.ca>
2018-03-18 12:18:16 -0600

    Added help text to file upload page

--- FSJ_django20_project/FSJ/forms_fileupload.py
+++ FSJ_django20_project/FSJ/forms_fileupload.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class FileUploadForm(forms.Form):
-    student_file = forms.FileField(required = False)
-    gpa_file = forms.FileField(required = False)
+    student_file = forms.FileField(required = False, help_text = ("Upload a CSV with fields CCID, ID, First Name, Last Name, Email (Univ), Prog & Year"))
+    gpa_file = forms.FileField(required = False, help_text = ("Upload a CSV with fields CCID & GPA"))
