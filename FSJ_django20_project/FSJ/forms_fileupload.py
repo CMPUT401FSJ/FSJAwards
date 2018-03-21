738f6b131c4c55909942cb1328375bf240de2b22
jrforest <jrforest@ualberta.ca>
2018-03-21 11:29:26 -0600

    Help text now translatable

--- FSJ_django20_project/FSJ/forms_fileupload.py
+++ FSJ_django20_project/FSJ/forms_fileupload.py
@@ -1,5 +1,6 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 class FileUploadForm(forms.Form):
-    student_file = forms.FileField(required = False, help_text = ("Upload a CSV with fields CCID, ID, First Name, Last Name, Email (Univ), Prog & Year"))
-    gpa_file = forms.FileField(required = False, help_text = ("Upload a CSV with fields CCID & GPA"))
+    student_file = forms.FileField(required = False, help_text = _("Upload a CSV with fields CCID, ID, First Name, Last Name, Email (Univ), Prog & Year"))
+    gpa_file = forms.FileField(required = False, help_text = _("Upload a CSV with fields CCID & GPA"))
