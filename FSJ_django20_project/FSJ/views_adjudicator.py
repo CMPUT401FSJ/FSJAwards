01fc0d8ac3e54f9185d561460cb1b8fdd92469a3
jrforest <jrforest@ualberta.ca>
2018-05-10 15:55:05 -0600

    Fixed comment list in view application

--- FSJ_django20_project/FSJ/views_adjudicator.py
+++ FSJ_django20_project/FSJ/views_adjudicator.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import redirect
+from django.conf import settings
+from django.contrib import messages
 from .models import *
 from .utils import *
 from .filters import *
@@ -193,4 +195,67 @@ def adjudicator_delete_comment(request, award_idnum, application_idnum):
         pass
     
     return redirect('adj_applicationlist', award_idnum = award_idnum)
-        
+
+@login_required
+@user_passes_test(is_adjudicator)
+def adjudicator_view_application(request):
+    application_id = request.GET.get("application_id", "")
+    FSJ_user = get_FSJ_user(request.user.username)
+    context = get_standard_context(FSJ_user)
+    return_url = request.GET.get("return", "")
+    url_is_safe = is_safe_url(url=urllib.parse.unquote(return_url),
+                              allowed_hosts=settings.ALLOWED_HOSTS,
+                              require_https=request.is_secure(), )
+
+    try:
+        application = Application.objects.get(application_id=application_id)
+        if application.is_archived:
+            return redirect('home')
+    except Application.DoesNotExist:
+        messages.warning(request, _("This application does not exist"))
+
+        if url_is_safe:
+            return redirect(urllib.parse.unquote(return_url))
+
+    if request.method == 'POST':
+        raise PermissionDenied
+
+    else:
+        try:
+            comment = Comment.objects.get(application=application, adjudicator=FSJ_user)
+            form = CommentRestrictedForm(instance=comment, prefix="form")
+
+            delete_url = "/adj_awardslist/" + str(application.award.awardid) + "/" + str(
+                application.application_id) + "/delete/"
+            context["delete_url"] = delete_url
+
+        except Comment.DoesNotExist:
+            form = CommentRestrictedForm(prefix="form")
+
+        try:
+            ranking = Ranking.objects.get(application=application, adjudicator=FSJ_user)
+            form2 = RankingRestrictedForm(FSJ_user, application.award, instance=ranking, prefix="form2")
+            context["ranking"] = ranking
+
+        except Ranking.DoesNotExist:
+            form2 = RankingRestrictedForm(FSJ_user, application.award, prefix="form2")
+
+        url = "/adj_awardslist/" + str(application.award.awardid) + "/" + str(application.application_id) + "/edit/"
+        context["form"] = form
+        context["form2"] = form2
+        context["adjudicator"] = FSJ_user
+
+        context["student"] = application.student
+        if application.application_file:
+            context["document"] = settings.MEDIA_URL + str(application.application_file)
+        context["award"] = application.award
+        context["url"] = url
+        if url_is_safe and return_url:
+            context["return_url"] = str(return_url)
+
+        context["archived"] = False
+        context["FSJ_user"] = FSJ_user
+        template = loader.get_template("FSJ/view_application.html")
+
+        application.add_viewed(FSJ_user)
+        return HttpResponse(template.render(context, request))
