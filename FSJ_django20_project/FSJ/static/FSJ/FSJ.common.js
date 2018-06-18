7e7c74dd4c2528743e82e67c4c098ba0640c6a73
jrforest <jrforest@ualberta.ca>
2018-06-18 13:17:15 -0600

    Aplhabetizing awards and adding select all

--- FSJ_django20_project/FSJ/static/FSJ/FSJ.common.js
+++ FSJ_django20_project/FSJ/static/FSJ/FSJ.common.js
@@ -6,3 +6,14 @@ function toggle(source) {
         }
     }
 }
+
+function award_toggle(source) {
+    var checkboxes = document.getElementsByName("awards");
+    //var checkboxes = $("input[name='awards']:checkbox");
+
+    for (var i = 0; i < checkboxes.length; i++) {
+        if ( !checkboxes[i].disabled ) {
+            checkboxes[i].checked = source.checked;
+        }
+    }
+}
